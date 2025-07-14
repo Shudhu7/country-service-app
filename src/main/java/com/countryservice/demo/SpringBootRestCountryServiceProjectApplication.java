2ca69e5ca454f9a0935a5514e1ea956accc0ccd3
Shudhu7 <mr.shudhuingle@gmail.com>
2025-07-14 21:00:18 +0530

    Initial commit - Spring Boot Country Service

---
+++ src/main/java/com/countryservice/demo/SpringBootRestCountryServiceProjectApplication.java
@@ -0,0 +1,14 @@
+package com.countryservice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestCountryServiceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestCountryServiceProjectApplication.class, args);
+	} 
+
+}
+ 
